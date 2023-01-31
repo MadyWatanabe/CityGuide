d8a9c4ebed272df581d7476db7e11a0ad313f311
MadyWatanabe <99287402+MadyWatanabe@users.noreply.github.com>
2023-01-30 21:42:01 -0600

    First commit

--- App.js
+++ App.js
@@ -1,20 +1,100 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet,View,FlatList,Image,Text } from 'react-native';
+import data from './attractions.json';
+import * as SplashScreen from 'expo-splash-screen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+SplashScreen.preventAutoHideAsync();
+setTimeout(SplashScreen.hideAsync, 2000);
+
+const chicagoIcon = require('./assets/icon.png');
+
+
+export default class App extends React.Component {
+  
+ 
+  // the parameter in renderRow must be "item" when working with a FlatList
+    renderRow({item}){
+
+     return (
+      
+       <View style={styles.row}>
+         <View style={styles.iconContainer}>
+           <Image source={chicagoIcon} style={styles.icon} />
+         </View>
+         <View style={styles.info}>
+           <Text style={styles.name}>{item.name}</Text>
+           <Text style={styles.address}>{item.address}</Text>
+          <Text style={styles.address}>{item.urlTag}</Text>
+         </View>
+       </View>
+     );
+   }
+   render() {
+    return (
+      <View style={styles.mainContainer}>
+        <Text style={styles.title}>City Guide</Text>
+        <FlatList data={data} renderItem={this.renderRow} />
+      </View>
+    );
+  }
+} 
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  mainContainer: {
+     flex: 1,
+     backgroundColor: '#ede2e0',
+   },
+   title: {
+     backgroundColor: '#1c7da5',
+     color: '#ede2e0',
+     fontSize: 18,
+     fontWeight: 'bold',
+     padding: 10,
+     paddingTop: 40,
+     textAlign: 'center',
+   },
+   row: {
+     borderColor: '#7b7983',
+     borderBottomWidth: 1,
+     flexDirection: 'row',
+     marginLeft: 10,
+     marginRight: 10,
+     paddingTop: 20,
+     paddingBottom: 20,
+   },
+   iconContainer: {
+     alignItems: 'center',
+     backgroundColor: '#1f3243',
+     borderColor: '#1f3243',
+     borderRadius: 45,
+     borderWidth: 1,
+     justifyContent: 'center',
+     height: 70,
+     width: 70,
+   },
+   icon: {
+     height: 45,
+     width: 45,
+   },
+    info: {
+     flex: 1,
+     paddingLeft: 25,
+     paddingRight: 25,
+   },
+   name: {
+    color:'122737',
+     fontWeight: 'bold',
+     fontSize: 18,
+     marginBottom: 5,
+   },
+   address: {
+     color: '#3c4f62',
+     fontSize: 16,
+   },
+   url: {
+    color: '#3c4f62',
+    fontSize: 16,
+    fontStyle: 'italic',
+   },
+ });
